518c0721b205b30c606909611a16939baec27a6a
David Clewett <david@clewett.se>
2014-12-23 01:54:04 +0100

    initial commit

---
+++ Clewtils/Extensions/StringUtilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clewtils.Extensions
+{
+    public static class StringUtilities
+    {
+        public static bool IsNumeric(this string s) {
+            float f = 0f;
+            return float.TryParse(s, out f);
+        }
+    }
+}
